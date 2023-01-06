ac462edaaaa4caac300ec9ddbc7cf7ae763e917b
Bartłomiej Szkodny <szkobart@outlook.com>
2023-01-06 22:38:18 +0100

    metody

--- First_project/src/Vehicle.java
+++ First_project/src/Vehicle.java
@@ -1,10 +1,16 @@
 
 public class Vehicle {
-
+	String czyjedzie() {
+		return "Jedzie";
+	}
+	String JakJedzie(String tekst) {
+		return tekst;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Vehicle Car = new Vehicle();
 		Vehicle Motor = new Vehicle();
+		System.out.println(Car.JakJedzie("Git")+Motor.czyjedzie());
 	}
 
 }
