b6c537801b8070c4613c238c7abc86bb0a860805
Bartłomiej Szkodny <szkobart@outlook.com>
2023-01-07 17:52:41 +0100

    update 3

--- First_project/src/Hello_world.java
+++ First_project/src/Hello_world.java
@@ -3,6 +3,8 @@ public class Hello_world {
 
 	public static void main(String[] args) {
 		System.out.println("Pierwszy program"+"w javie");
+		System.out.println("Druga metoda");
+		System.out.println("Trzecia metoda");
 		
 	}
 
