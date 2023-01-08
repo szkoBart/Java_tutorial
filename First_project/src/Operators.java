bbfcd60dce6d63b2f6ff6463c62b2cd31ee9cf8a
Bartłomiej Szkodny <szkobart@outlook.com>
2023-01-08 12:24:10 +0100

    Operators

---
+++ First_project/src/Operators.java
@@ -0,0 +1,18 @@
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int a =5;
+		int b =11;
+		System.out.println(a+b);
+		System.out.println(5+11);
+		System.out.println(a-b);
+		System.out.println(a/b);
+		System.out.println(a*b);
+		System.out.println(b%a);
+		int c= 0;
+		c++;
+		System.out.println(c);
+	}
+
+}
