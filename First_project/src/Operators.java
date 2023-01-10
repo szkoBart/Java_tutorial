bbe67da8167b6247f4ad86ea69a54e8a3e0c240b
Bartłomiej Szkodny <szkobart@outlook.com>
2023-01-10 22:59:22 +0100

    update operatory przypisania

--- First_project/src/Operators.java
+++ First_project/src/Operators.java
@@ -2,26 +2,17 @@
 public class Operators {
 
 	public static void main(String[] args) {
-		int a =5;
-		int b =11;
-		System.out.println(a+b);
-		System.out.println(5+11);
-		System.out.println(a-b);
-		System.out.println(a/b);
-		System.out.println(a*b);
-		System.out.println(b%a);
-		int c= 0;
-		c++;
-		System.out.println(c);
-		System.out.println(a<b);
-		System.out.println(b<a);
-		System.out.println((a*b)<=5);
-		boolean t = true;
-		boolean f = false;
-		System.out.println(t||f);
-		System.out.println(t&&f);
-		System.out.println(!f);
-		System.out.println(a<b&&t);
+		int a = 5;
+		int b = 10;
+		a *=10;
+		a/=b;
+		a%=b;
+		a+=b;
+		a-=b;
+		boolean c = true;
+		boolean d = false;
+		c &= d;
+		c|= d;
 	}
 
 }
